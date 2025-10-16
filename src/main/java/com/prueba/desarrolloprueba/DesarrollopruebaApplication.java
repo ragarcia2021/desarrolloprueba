0ab5a9fd787b2f46cb48b11a3f00ea059e483bf6
Rafael Garcia Torres <ragarcia@ikeasistencia.com>
2025-10-16 10:30:46 -0600

    Se carga version inicial del proyecto

---
+++ src/main/java/com/prueba/desarrolloprueba/DesarrollopruebaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.desarrolloprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrollopruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrollopruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/desarrolloprueba/DesarrollopruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.desarrolloprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrollopruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
